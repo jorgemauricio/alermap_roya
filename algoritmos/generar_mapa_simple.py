bd8e4ae0e047278b21b3338d82ac3de522243cc6
jorgemauricio <jorge.ernesto.mauricio@gmail.com>
2018-02-22 19:49:00 -0600

    generar primer versión de mapa

--- algoritmos/generar_mapa_simple.py
+++ algoritmos/generar_mapa_simple.py
@@ -24,11 +24,10 @@ import ftplib
 import shutil
 import csv
 import math
-from api import claves
 
 def main():
 	print("Init")
-	mapasExtremos()
+	mapaRoya()
 
 def descargarInfo():
 	# datos del servidor
@@ -140,13 +139,13 @@ def generarTexto(f, k,vMn, vMx):
 	else:
 		pass
 
-def mapasExtremos():
+def mapaRoya():
 	"""
 	Función que permite generar los mapas de eventos extremos
 	"""
 	# ********** fecha pronóstico
 	# fechaPronostico = fp
-	fechaPronostico = "2018-02-22"
+	fechaPronostico = "2018-02-21"
 	# fechaPronostico = strftime("%Y-%m-%d")
 
 	# ********** path
@@ -163,137 +162,164 @@ def mapasExtremos():
 	LAT_MIN = 12.37
 
 	# ********** Path
-	path = "/Users/jorgemauricio/Documents/Research/alermapweb"
+	# Mac : path = "/Users/jorgemauricio/Documents/Research/alermap_roya"
+	# Linux : path = "/home/jorge/Documents/Research/alermap_roya"
+	path = "/home/jorge/Documents/Research/alermap_roya"
 	os.chdir(path)
-
-	# ********** dict de análisis
-	d = {"Rain" : '20/500', "Tmax": '30/60', "Tmin" : '-9/6', "Windpro" : '62/150'}
 
 	# ********** array colores
 	# generar fechas mediante función
 	arrayFechas = generarFechas(fechaPronostico)
 
 	# leer csv
-	for j in range(1, 6, 1):
-		pathFile = '{}/data/{}/d{}.txt'.format(path,fechaPronostico,j)
-		data = pd.read_table(pathFile, sep=',')
+	dataTemp = "{}/data/{}/d1.txt".format(path,fechaPronostico)
+	dataTemp2 = "{}/data/{}/d2.txt".format(path,fechaPronostico)
+	dataTemp3 = "{}/data/{}/d3.txt".format(path,fechaPronostico)
+	dataTemp4 = "{}/data/{}/d4.txt".format(path,fechaPronostico)
+	dataTemp5 = "{}/data/{}/d5.txt".format(path,fechaPronostico)
 
-		for key, i in d.items():
-			# comenzar con el proceso
-			tiempoInicio = strftime("%Y-%m-%d %H:%M:%S")
-			print("Empezar procesamiento {} {} tiempo: {}".format(key, i, tiempoInicio))
+	data = pd.read_csv(dataTemp)
+	data2 = pd.read_csv(dataTemp2)
+	data3 = pd.read_csv(dataTemp3)
+	data4 = pd.read_csv(dataTemp4)
+	data5 = pd.read_csv(dataTemp5)
 
-			# obtener rangos
-			vMin, vMax = i.split('/')
-			vMin = int(vMin)
-			vMax = int(vMax)
+	data["Tmax1"] = data["Tmax"]
+	data["Tmax2"] = data2["Tmax"]
+	data["Tmax3"] = data3["Tmax"]
+	data["Tmax4"] = data4["Tmax"]
+	data["Tmax5"] = data5["Tmax"]
 
-			# título temporal de la columna a procesar
-			tituloTemporalColumna = key
-			dataTemp = data
+	data["Tmin1"] = data["Tmin"]
+	data["Tmin2"] = data2["Tmin"]
+	data["Tmin3"] = data3["Tmin"]
+	data["Tmin4"] = data4["Tmin"]
+	data["Tmin5"] = data5["Tmin"]
 
-			#obtener valores de x y y
-			lons = np.array(dataTemp['Long'])
-			lats = np.array(dataTemp['Lat'])
+	data["Dpoint1"] = data["Dpoint"]
+	data["Dpoint2"] = data2["Dpoint"]
+	data["Dpoint3"] = data3["Dpoint"]
+	data["Dpoint4"] = data4["Dpoint"]
+	data["Dpoint5"] = data5["Dpoint"]
 
-			#%% set up plot
-			plt.clf()
+	data["Tpro1"] = data["Tpro"]
+	data["Tpro2"] = data2["Tpro"]
+	data["Tpro3"] = data3["Tpro"]
+	data["Tpro4"] = data4["Tpro"]
+	data["Tpro5"] = data5["Tpro"]
 
-			fig = plt.figure(figsize=(8,4))
-			m = Basemap(projection='mill',llcrnrlat=LAT_MIN,urcrnrlat=LAT_MAX,llcrnrlon=LONG_MIN,urcrnrlon=LONG_MAX,resolution='h', area_thresh = 10000)
+	data["indice1"] = data.apply(lambda x: generarIndice(x["Tmax1"],x["Tpro1"],x["Tmin1"],x["Dpoint1"]), axis=1).astype(int)
+	data["indice2"] = data.apply(lambda x: generarIndice(x["Tmax2"],x["Tpro2"],x["Tmin2"],x["Dpoint2"]), axis=1).astype(int)
+	data["indice3"] = data.apply(lambda x: generarIndice(x["Tmax3"],x["Tpro3"],x["Tmin3"],x["Dpoint3"]), axis=1).astype(int)
+	data["indice4"] = data.apply(lambda x: generarIndice(x["Tmax4"],x["Tpro4"],x["Tmin4"],x["Dpoint4"]), axis=1).astype(int)
+	data["indice5"] = data.apply(lambda x: generarIndice(x["Tmax5"],x["Tpro5"],x["Tmin5"],x["Dpoint5"]), axis=1).astype(int)
 
-			# # # # # # # # # #
-			# generar lats, lons
-			x, y = m(lons, lats)
+	data["indiceTotal"] = data.apply(lambda x: generarIncideTotal(x["indice1"],x["indice2"],x["indice3"],x["indice4"],x["indice5"]), axis=1)
 
-			# numero de columnas y filas
-			numCols = len(x)
-			numRows = len(y)
+	data["indiceNumero"] = data.apply(lambda x: generarNumero(x["indiceTotal"]), axis=1)
 
-			# generar xi, yi
-			xi = np.linspace(x.min(), x.max(), 1000)
-			yi = np.linspace(y.min(), y.max(), 1000)
+	print(data.head())# comenzar con el proceso
+	tiempoInicio = strftime("%Y-%m-%d %H:%M:%S")
+	print("Empezar procesamiento tiempo: {}".format(tiempoInicio))
 
-			# generar el meshgrid
-			xi, yi = np.meshgrid(xi, yi)
+	#obtener valores de x y y
+	lons = np.array(data['Long'])
+	lats = np.array(data['Lat'])
 
-			# generar zi
-			z = np.array(dataTemp[key])
-			zi = gd((x,y), z, (xi,yi), method='cubic')
-			#zi = gd((x,y), z, (xi,yi))
+	#%% set up plot
+	plt.clf()
 
-			# agregar shape
-			m.readshapefile('shapes/Estados', 'Estados')
+	fig = plt.figure(figsize=(8,4))
+	m = Basemap(projection='mill',llcrnrlat=LAT_MIN,urcrnrlat=LAT_MAX,llcrnrlon=LONG_MIN,urcrnrlon=LONG_MAX,resolution='h', area_thresh = 10000)
 
-			# clevs
-			clevs = generarRangos(key)
+	# # # # # # # # # #
+	# generar lats, lons
+	x, y = m(lons, lats)
 
-			# contour plot
-			cs = m.contourf(xi,yi,zi, clevs, zorder=25, alpha=0.7, cmap=generarMapaDeColores(key))
+	# numero de columnas y filas
+	numCols = len(x)
+	numRows = len(y)
 
-			# colorbar
-			cbar = m.colorbar(cs, location='right', pad="5%")
+	# generar xi, yi
+	xi = np.linspace(x.min(), x.max(), 1000)
+	yi = np.linspace(y.min(), y.max(), 1000)
 
-			# simbología
-			simbologia = generarSimbologia(key)
-			cbar.set_label(simbologia)
+	# generar el meshgrid
+	xi, yi = np.meshgrid(xi, yi)
 
-			# titulo del mapa
-			tituloTemporalMapa = generarTexto(arrayFechas[j-1], key, vMin, vMax)
-			plt.title(tituloTemporalMapa)
+	# generar zi
+	z = np.array(data["indiceNumero"])
+	zi = gd((x,y), z, (xi,yi), method='cubic')
+	#zi = gd((x,y), z, (xi,yi))
 
-			tituloTemporalArchivo = "{}/data/{}/{}_{}_{}_{}.png".format(path,fechaPronostico,arrayFechas[j-1],key,vMin, vMax)
+	# agregar shape
+	m.readshapefile('shapes/Estados', 'Estados')
 
-			# crear anotación
-			latitudAnotacion = (LAT_MAX + LAT_MIN) / 2
-			longitudAnotacion = (LONG_MAX + LONG_MIN) / 2
-			plt.annotate('@2018 INIFAP', xy=(longitudAnotacion,latitudAnotacion), xycoords='figure fraction', xytext=(0.45,0.45), color='g')
+	# clevs
+	clevs = [1,2,3,4,5,6,7,8,9,10]
 
-			# guardar mapa
-			plt.savefig(tituloTemporalArchivo, dpi=300)
-			print('****** Genereate: {}'.format(tituloTemporalArchivo))
+	# contour plot
+	cs = m.contourf(xi,yi,zi, clevs, zorder=25, alpha=0.7, cmap='RdYlGn_r')
 
-			# finalizar con el proceso
-			tiempoFinal = strftime("%Y-%m-%d %H:%M:%S")
-			print("Terminar procesamiento {} {} tiempo: {}".format(key, i, tiempoInicio))
+	# colorbar
+	cbar = m.colorbar(cs, location='right', pad="5%")
+
+	# simbología
+	cbar.set_label("")
+
+	# titulo del mapa
+	plt.title("Índice de Presencia de Roya")
+
+	tituloTemporalArchivo = "{}/data/{}/{}_Roya.png".format(path,fechaPronostico,arrayFechas[0])
+
+	# crear anotación
+	latitudAnotacion = (LAT_MAX + LAT_MIN) / 2
+	longitudAnotacion = (LONG_MAX + LONG_MIN) / 2
+	plt.annotate('@2018 INIFAP', xy=(longitudAnotacion,latitudAnotacion), xycoords='figure fraction', xytext=(0.45,0.45), color='g')
+
+	# guardar mapa
+	plt.savefig(tituloTemporalArchivo, dpi=300)
+	print('****** Genereate: {}'.format(tituloTemporalArchivo))
+
+	# finalizar con el proceso
+	tiempoFinal = strftime("%Y-%m-%d %H:%M:%S")
+	print("Terminar procesamiento tiempo: {}".format(tiempoInicio))
 
 
-def generarRangos(variable):
-	if variable == "Rain":
-		return (20, 50, 70, 100, 150,200, 300, 400, 500)
-	elif variable == "Tmax":
-		return (30, 35,40, 45, 50, 55, 60)
-	elif variable == "Tmin":
-		return (-9, -6, -3, 0, 3, 6)
-	elif variable == "Windpro":
-		return (62, 74, 88, 102, 117, 150)
+def generarIndice(tmax, tpro, tmin, dpoint):
+	tmidnight = tmax - tmin
+	if tpro > 25 and tpro < 30 and tmidnight > 15 and tmidnight < 20 and dpoint >5:
+		return 1
 	else:
-		print("Error Clevs")
+		return 0
 
-def generarSimbologia(variable):
-	if variable == "Rain":
-		return "mm"
-	elif variable == "Tmax":
-		return "ºC"
-	elif variable == "Tmin":
-		return "ºC"
-	elif variable == "Windpro":
-		return "km/h"
+def generarIncideTotal(i1, i2, i3, i4, i5):
+	indiceTotal = "{}{}{}{}{}".format(int(i1),int(i2),int(i3),int(i4),int(i5))
+	return indiceTotal
+
+def generarNumero(indiceTotal):
+	if indiceTotal == "11111":
+		return 10
+	elif indiceTotal == "11110":
+		return 9
+	elif indiceTotal == "01111":
+		return 8
+	elif indiceTotal == "11100":
+		return 7
+	elif indiceTotal == "01110":
+		return 6
+	elif indiceTotal == "00111":
+		return 5
+	elif indiceTotal == "11000":
+		return 4
+	elif indiceTotal == "01100":
+		return 3
+	elif indiceTotal == "00110":
+		return 2
+	elif indiceTotal == "00011":
+		return 1
 	else:
-		print("Error Clevs")
-
-def generarMapaDeColores(variable):
-	if variable == "Rain":
-		return "rainbow"
-	elif variable == "Tmax":
-		return "YlOrRd"
-	elif variable == "Tmin":
-		return "winter"
-	elif variable == "Windpro":
-		return "Wistia"
-	else:
-		print("Error Clevs")
-
+		return 0
 
 if __name__ == '__main__':
     main()
